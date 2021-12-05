f8ec6afae9aa2a1ac6b43dfcb3e9892105937168
Quinn Tao <qintutao@gmail.com>
2021-12-04 19:31:27 -0800

    dev: added basic structure for timeline-card component

--- src/components/TimelineCardComponent.js
+++ src/components/TimelineCardComponent.js
@@ -2,31 +2,67 @@
 // @last update on Dec 4, 2021
 
 import React from 'react';
-import PropTypes, { func } from 'prop-types';
+import PropTypes from 'prop-types';
+import "../style/TimelineCardStyle.css";
 
 /**
  * A Card that renders details of a timeline event
  */
 function TimelineCard(props) {
-
+    return(
+        <div className="timeline-card-container">
+            {_renderTime(props.initdate, props.termdate)}
+            <h1 className="timeline-card-title">{props.title ? props.title : "Unamed Project"}</h1>
+            {props.brief ? <p className="timeline-card-brief">{props.brief}</p> : null}
+            {props.organization ? <p className="timeline-card-organization">{props.organization}</p> : null}
+            {_renderTags(props.tags)}
+            {_renderDescription(props.description)}
+            {_renderLinks(props.demolink, props.sourcelink)}
+        </div>
+    );
 }
 
-TimelineCard.prototype = {
+// Props specification 
+TimelineCard.propTypes = {
     title:          PropTypes.string,
     brief:          PropTypes.string,
     organization:   PropTypes.string, 
+    initdate:       PropTypes.string,                       // Project initiation date
+    termdate:       PropTypes.string,                       // Project termination date
     tags:           PropTypes.arrayOf(PropTypes.string),    // Tech tags 
     description:    PropTypes.arrayOf(PropTypes.string),    // a list of one-sentence description 
-    demolink:       function(props, propname) {return simpleHTTPSVerifier(props[propname])},
-    sourcelink:     function(props, propname) {return simpleHTTPSVerifier(props[propname])}
+    demolink:       function(props, propname) {return _simpleHttpsVerifier(props[propname])},
+    sourcelink:     function(props, propname) {return _simpleHttpsVerifier(props[propname])}
 }
-
-// Very simple HTTP Url checker,
-//   may be expanded later ...
-function simpleHTTPSVerifier(input) {
-    if (!(/https:\/\/.*/.test(input))) {
+/* ============================= Helper Functions =============================== */
+// Very simple HTTP Url checker, may be expanded later ...
+function _simpleHttpsVerifier(input) {
+    if (input != null && !(/https:\/\/.*/.test(input))) {
         return new Error("Not a valid https URL");
     } 
+}
+
+function _renderTime(initdate, termdate) {
+    return (
+        <div className="timeline-card-time-container">
+            <p>{initdate} to {termdate}</p>
+        </div>
+    )
+}
+
+// @returns description list in <li> form
+function _renderDescription(description) {
+    return null; // stub
+}
+
+// @returns rendered tech tags in array form
+function _renderTags(tags) {
+    return null; // stub
+}
+
+// @returns links in <a> form with specific icons
+function _renderLinks(demolink, sourcelink) {
+    return null; // stub
 }
 
 export default TimelineCard;
--- src/components/TimelinePortfolioComponent.js
+++ src/components/TimelinePortfolioComponent.js
@@ -2,13 +2,21 @@
 // @last update on Dec 4, 2021
 
 import React from 'react';
+import TimelineCard from './TimelineCardComponent';
 
 
 /**
  * A Portfolio Component that relies on timelineData.json to render 
  */
 function TimelinePortfolio(props) {
-    
+    return (
+        <TimelineCard 
+            title="This Website"
+            brief="A simple personal static portfolio website built with React"
+            tags={["ReactJS"]}
+            demolink="https://ggbond.com"
+        ></TimelineCard>
+    );
 }
 
 export default TimelinePortfolio;
