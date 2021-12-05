b1ce15ff6e6bd4d96b2b4a8f71697e8342c0f919
Quinn Tao <qintutao@gmail.com>
2021-12-04 19:48:42 -0800

    dev: added cdn use google font: noto sans sc

--- src/components/TimelinePortfolioComponent.js
+++ src/components/TimelinePortfolioComponent.js
@@ -4,7 +4,6 @@
 import React from 'react';
 import TimelineCard from './TimelineCardComponent';
 
-
 /**
  * A Portfolio Component that relies on timelineData.json to render 
  */
@@ -12,8 +11,10 @@ function TimelinePortfolio(props) {
     return (
         <TimelineCard 
             title="This Website"
+            initdate="Dec 4"
+            termdate="Present"
             brief="A simple personal static portfolio website built with React"
-            tags={["ReactJS"]}
+            tags={["React", "Javascript", "CSS", "HTML"]}
             demolink="https://ggbond.com"
         ></TimelineCard>
     );
